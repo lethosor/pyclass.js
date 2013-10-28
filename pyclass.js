a49b5d50e286ecbcc7a8f9b6b7072b2be7a6b137
lethosor <stahlalan19@gmail.com>
2013-10-28 16:52:51 -0400

    Basic instantiation

--- pyclass.js
+++ pyclass.js
@@ -1,9 +1,26 @@
 window.Class = (function(){
 	"use strict";
+	
+	var BaseClass = function(){
+		var self = {};
+		self.__init__ = function(){};
+		return self;
+	};
+	
 	var Class = function(){
 		var classes = [].slice.apply(arguments),
-			constructor = classes.pop();
-	}
+			constructor = classes.pop(),
+			object = {},
+			mro = [];
+		return function(){
+			constructor(object);
+			if ('__init__' in object && 'apply' in object.__init__)
+				object.__init__.apply(this, arguments);
+			return object;
+		};
+	};
+	
+	Class.BaseClass = BaseClass;
 	
 	return Class;
-});
+})();
